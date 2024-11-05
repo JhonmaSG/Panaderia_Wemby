86658a8661c96e373195f1d808131a7e9ba91657
Santiago Riano <santiagoriano68@gmail.com>
2024-11-05 12:48:21 -0500

    compatibilidad con joel parte 1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,31 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//sprint inventario
+
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+
+Route::get('/productos/registrar', [ProductoController::class, 'create'])->name('productos.create');
+
+Route::post('/productos/registrar', [ProductoController::class, 'store'])->name('productos.store');
+
+Route::get('/productos/{id}/editar-stock', [ProductoController::class, 'editStock'])->name('productos.edit-stock');
+
+Route::post('/productos/{id}/actualizar-stock', [ProductoController::class, 'updateStock'])->name('productos.update-stock');
+
+Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
+
+
+//sprint ventas
+
+Route::get('/ventas/crear-venta', [VentaController::class, 'create'])->name('ventas.create');
+Route::post('/ventas/crear-venta', [VentaController::class, 'store'])->name('ventas.store');
+
+//sprint reportes y análisis
+
 Route::get('/reports', [GraficosController::class, 'ventas'])->name('graficos.ventas');
 Route::get('/analisis', [AnalisisController::class, 'showAnalysisForm'])->name('analisis.form');
 Route::post('/analisis/generar', [AnalisisController::class, 'generateAnalysis'])->name('analisis.generate');
